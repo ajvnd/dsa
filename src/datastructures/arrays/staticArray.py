c468276e750397a7e97c02fa8487f06ca4f3ad0f
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-07-08 18:49:46 +0330

    Added some comments for readability of code.

--- src/datastructures/arrays/staticArray.py
+++ src/datastructures/arrays/staticArray.py
@@ -1,12 +1,14 @@
 class StaticArray():
-    def __init__(self, capacity, item_type):
+    def __init__(self, item_type, capacity):
 
+        # make sure the capacity is greater or equal to zero
         if capacity <= 0:
             raise ValueError("Capacity must be a positive integer")
 
         self._capacity = capacity
         self._container = [None] * capacity
 
+        # make sure the data type of items is a primitive data type
         if item_type not in [int, float, str, bool]:
             raise ValueError("Item type must be a primitive data type")
 
@@ -19,6 +21,7 @@ class StaticArray():
     def __setitem__(self, index, value):
         self.__check_index(index)
 
+        # Check if the provided value matches the expected type for elements in the array.
         if not isinstance(value, self.__item_type):
             raise TypeError(f"Expected item of type {self.__item_type.__name__}, but got {type(value).__name__}")
 
@@ -38,13 +41,23 @@ class StaticArray():
         return sum(1 for x in self._container if x is not None)
 
     def __check_index(self, index):
-        if not isinstance(index, int) or index < 0 or index >= self._capacity:
+        is_integer = isinstance(index, int)
+        is_less_then_zero = index < 0
+        is_grater_equal_to_capacity = index >= self._capacity
+
+        # Check if the index is integer  value matches the expected type for elements in the array.
+        if not is_integer or is_less_then_zero or is_grater_equal_to_capacity:
             raise IndexError("Index out of range")
+
+    def get_index(self, value):
+        for item in self._container:
+            if item is not None and item == value:
+                return item
 
 
 if __name__ == '__main__':
 
-    array = StaticArray(5, str)
+    array = StaticArray(str, 5)
 
     array[0] = "A"
     array[1] = "B"
