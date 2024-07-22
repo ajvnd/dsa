953b9d2df340f85550034b8946e7b5260c4ad4e0
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-07-23 01:54:17 +0330

    initial commit

---
+++ tests/datastructures/arrays/test_static_array.py
@@ -0,0 +1,30 @@
+import pytest
+
+from src.datastructures.arrays.static_array import StaticArray
+
+
+class TestStaticArray:
+
+    @pytest.fixture
+    def static_array(self):
+        return StaticArray(int, 5)
+
+    def test_can_initialize_fields_correctly(self, static_array):
+        # arrange
+
+        # act
+
+        # assert
+        assert isinstance(static_array, StaticArray)
+        assert static_array._capacity == 5
+        assert static_array.__dict__['_StaticArray__item_type'] == int
+
+    def test_can_set_items_correctly(self, static_array):
+        # assert 
+        new_item = 5
+
+        # act
+        static_array[0] = new_item
+
+        # assert
+        assert static_array[0] == new_item
