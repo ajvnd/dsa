1fa38e728c0c04b75a613cea43a7b6e7898cc275
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-07-27 19:36:49 +0330

    initial commit

---
+++ src/datastructures/linkedlist/doubly_linkedlist.py
@@ -0,0 +1,28 @@
+from common.linkedlist import LinkedList
+
+
+class DoublyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList(LinkedList):
+    def __init__(self):
+        super().__init__()
+
+    def add_first(self, value):
+        pass
+
+    def add_last(self, value):
+        pass
+
+    def add_at(self, index, value):
+        pass
+
+    def remove_last(self):
+        pass
+
+    def remove_at(self, index):
+        pass
