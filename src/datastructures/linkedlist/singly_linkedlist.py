252604ba1a3d4506dae71107187cb9f531dea4c1
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-07-27 19:37:14 +0330

    cleand up common code that are exists in super class.

--- src/datastructures/linkedlist/singly_linkedlist.py
+++ src/datastructures/linkedlist/singly_linkedlist.py
@@ -1,7 +1,5 @@
-class EmptyLinkedListException(Exception):
-    def __init__(self, message="The linked list is empty."):
-        self.message = message
-        super().__init__(self.message)
+from common.empty_linkedlist_exception import EmptyLinkedListException
+from common.linkedlist import LinkedList
 
 
 class SinglyLinkedListNode:
@@ -10,14 +8,13 @@ class SinglyLinkedListNode:
         self.next = None
 
 
-class SinglyLinkedList:
+class SinglyLinkedList(LinkedList):
     def __init__(self):
-        self.head = None
-        self.tail = None
+        super().__init__()
 
     def add_first(self, value):
         if self.is_empty():
-            self.__initialize_linkedlist(value)
+            self._initialize_linkedlist(SinglyLinkedListNode(value))
         else:
             node = SinglyLinkedListNode(value)
             node.next = self.head
@@ -25,7 +22,7 @@ class SinglyLinkedList:
 
     def add_last(self, value):
         if self.is_empty():
-            self.__initialize_linkedlist(value)
+            self._initialize_linkedlist(SinglyLinkedListNode(value))
         else:
             self.tail.next = SinglyLinkedListNode(value)
             self.tail = self.tail.next
@@ -33,41 +30,18 @@ class SinglyLinkedList:
     def add_at(self, index, value):
         pass
 
-    def remove_first(self):
-        if self.is_empty():
-            raise EmptyLinkedListException()
-        self.head = self.head.next
-
     def remove_last(self):
         if self.is_empty():
             raise EmptyLinkedListException()
 
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+
+        previous_to_tail = self._getitem(self.__len__() - 2)
+        previous_to_tail.next = None
+        self.tail = previous_to_tail
+
     def remove_at(self, index):
         if self.is_empty():
             raise EmptyLinkedListException()
-    def is_empty(self):
-        return self.head is None and self.tail is None
-
-    def peak_first(self):
-        if self.is_empty():
-            raise EmptyLinkedListException()
-        return self.head.data
-
-    def peak_last(self):
-        if self.is_empty():
-            raise EmptyLinkedListException()
-        return self.tail.data
-
-    def __initialize_linkedlist(self, value):
-        self.head = SinglyLinkedListNode(value)
-        self.tail = self.head
-
-    def __len__(self):
-        counter = 0
-        current_node = self.head
-
-        while current_node is not None:
-            current_node = current_node.next
-            counter += 1
-
-        return counter
